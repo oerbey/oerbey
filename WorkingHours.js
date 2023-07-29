9d04bc68b9a1e765114c3756cc406b19bc33d1ef
oerbey <66864050+oerbey@users.noreply.github.com>
2023-07-29 11:58:30 +0300

    More days

--- WorkingHours.js
+++ WorkingHours.js
@@ -1,6 +1,14 @@
 const officialHolidays = [
     { date: '2022-01-01', name: 'New Year' },
-    { date: '2022-07-04', name: 'Independence Day' },
+    { date: '2022-05-01', name: 'Labour Day' },
+    { date: '2022-05-04', name: 'Declaration of Independence Day' },
+    { date: '2022-06-24', name: 'Ligo' },
+    { date: '2022-06-25', name: 'Jani' },
+    { date: '2022-07-10', name: 'Dancing Festival' },
+    { date: '2022-12-24', name: 'Christmas Eve' },
+    { date: '2022-12-25', name: 'Christmas Christmas' },
+    { date: '2022-12-26', name: 'Second day of Christmas' },
+    { date: '2022-12-24', name: 'New Years Eve' },
     // Add more holidays as needed
   ];
   
